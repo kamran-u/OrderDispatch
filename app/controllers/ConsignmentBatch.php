5adfd0ac09728db8428b0d293dd2cafdf67b9e68
kamran ullah <kamranullah@hotmail.com>
2020-06-08 11:18:56 +0100

    Consigments dispatch working hours

--- app/controllers/ConsignmentBatch.php
+++ app/controllers/ConsignmentBatch.php
@@ -8,12 +8,22 @@ use \Carbon\Carbon;
 
 class ConsignmentBatch
 {
+
     /**
      * Start new batch or continue started batch
      * @return array
      */
     public function start()
     {
+        // this functionality should be on route middleware
+        if(!$this->batchWorkingHours())
+        {
+            return [
+                    'status' => 403,
+                    'msg' => 'Batch unavailable. Out of working hours'
+                ];
+        }
+
         //check if batch already started if not the create
         $batch = Batch::whereDate('created_at', Carbon::today())->select(['id', 'deleted_at', 'closed_at'])->get();
         $closed_at = $batch->pluck('closed_at')->first();
@@ -44,11 +54,20 @@ class ConsignmentBatch
 
     /**
      * Dispatch consignment ids to relevant couriers
-     * close the batch 
+     * Close the batch by setting up the date 
      * @return array
      */
     public function end($batchId)
     {
+        // this functionality should be on route middleware
+        if(!$this->batchWorkingHours())
+        {
+            return [
+                    'status' => 403,
+                    'msg' => 'Batch unavailable. Out of working hours'
+                ];
+        }
+
         $consignments = Consignment::where('batch_id', $batchId)->get(['courier_id', 'consignment_id'])
                                                                 ->groupBy('courier_id')
                                                                 ->toArray();
@@ -58,5 +77,14 @@ class ConsignmentBatch
         Batch::find($batchId)->update(['closed_by' => loggedInUserId(), 'closed_at' => now()]);
         
         return $dispatchStatus;
+    }
+
+    /**
+     * logic for batch working hours
+     * @return boolean
+     */
+    protected function batchWorkingHours()
+    {
+        return 1;
     }
 }
